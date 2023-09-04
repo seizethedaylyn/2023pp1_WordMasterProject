24f3a5c421660fc42d4a43fe72a4846b06db1f3b
seizethedaylyn <22000189@handong.ac.kr>
2023-09-05 02:23:16 +0900

    add function list

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -35,7 +35,7 @@ public class WordManager {
                 wordCRUD.addWord();
             }
             else if(menu == 1){
-                //list
+                wordCRUD.listAll();
             }
         }
     }
